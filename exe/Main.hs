ae66b0c5508721a0084b3d7262615dded6dfd178
Ignat Insarov <kindaro@gmail.com>
2019-10-01 12:16:59 +0300

    Have the turtle walk.

--- exe/Main.hs
+++ exe/Main.hs
@@ -3,7 +3,13 @@ module Main where
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.Foldable
+import qualified Data.List as List
+import Data.Function
 
+import Text.PrettyPrint.Boxes
+
+import Zipper (Zipper(..), Zipper2D)
+import qualified Zipper as Zipper
 import Grammar
 import Generator
 
@@ -36,7 +42,47 @@ teaParty = grammar nonTerminals terminals rules start
               ]
     start = "names"
 
+data Direction = North | East | South | West deriving (Show, Eq, Ord, Enum, Bounded)
+data Move = Move deriving (Show, Eq, Ord)
+
+chrDirection :: Direction -> Char
+chrDirection North = '⭡'
+chrDirection East  = '⭢'
+chrDirection South = '⭣'
+chrDirection West  = '⭠'
+
+growDirection :: a -> Direction -> Zipper2D a -> Zipper2D a
+growDirection x North = Zipper.growUp2D    x
+growDirection x East  = Zipper.growRight2D  x
+growDirection x South = Zipper.growDown2D  x
+growDirection x West  = Zipper.growLeft2D x
+
+turtle :: Grammar Move Direction
+turtle = grammar [Move] [minBound..] rules Move
+  where
+    rules = Set.fromList $
+        [ [Left Move] := [Right North, Left Move, Right South]
+        , [Left Move] := [Right East, Left Move, Right West]
+        , [Left Move] := [ ]
+        ] ++ [ [Right x, Right y]
+             := [Right y, Right x] | x <- [minBound..], y <- List.delete x [minBound..] ]
+
+drawTurtle :: Sentence Direction -> [String]
+drawTurtle s = fmap (Zipper.tabulate2d pure) $ List.scanl walk (Zipper.singleton2d '+') s
+  where
+    walk :: Zipper2D Char -> Direction -> Zipper2D Char
+    walk z x = z & growDirection ' ' x
+                 & Zipper.extend2d (const (chrDirection x))
+                 & growDirection ' ' x
+                 & Zipper.extend2d (\z -> if Zipper.extract2d z == '+' then '+' else '*')
+
+renderTurtle :: Sentence Direction -> String
+renderTurtle = render . Zipper.frame . Zipper.box . last . drawTurtle
+
 main :: IO ()
 main = do
-    print $ take 10 (generate exampleGrammar)
-    traverse_ (putStrLn . concat) . take 10 . generate $ teaParty
+    -- print $ take 10 (generate exampleGrammar)
+    -- traverse_ (putStrLn . concat) . take 10 . generate $ teaParty
+    -- traverse_ print . take 10 . drop 100 . generate $ turtle
+    traverse_ ((\(x, (y, z)) -> print x >> print y >> putStrLn z) . fmap (\x -> (x, renderTurtle x)))
+        . take @(Int, Sentence Direction) 1000 . drop 10000 $ zip [1 :: Int ..] (generate turtle)
--- lib/Zipper.hs
+++ lib/Zipper.hs
@@ -11,6 +11,12 @@ data Zipper a = Zipper { lefts, rights :: [a], focus :: a } deriving Eq
 integers :: Integral i => Zipper i
 integers = Zipper { lefts = [-1, -2..], focus = 0, rights = [1..] }
 
+singleton :: a -> Zipper a
+singleton x = Zipper { lefts = [ ], focus = x, rights = [ ] }
+
+mirror :: a -> [a] -> Zipper a
+mirror x xs = Zipper { lefts = xs, focus = x, rights = xs }
+
 instance Functor Zipper where
     fmap f Zipper{..} = Zipper { lefts = fmap f lefts, focus = f focus, rights = fmap f rights }
 
@@ -22,7 +28,11 @@ instance Show a => Show (Zipper a) where
       where
         cellX = maximum . fmap (maximum . fmap length . lines . show) $ z
         cellY = maximum . fmap (length . lines . show) $ z
-        boxify = tabulate cellX cellY (length lefts) (length rights) id
+        boxify = tabulate show cellX cellY (length lefts) (length rights) id
+
+data X = X | Y
+instance Show X where show X = "la\nla"; show Y = "fa"
+z1 = Zipper { lefts = [X], focus = Y, rights = [ ] }
 
 frame :: Box -> Box
 frame x = hcat @[] center1 [vstick, hstick // x // hstick, vstick]
@@ -30,12 +40,19 @@ frame x = hcat @[] center1 [vstick, hstick // x // hstick, vstick]
     vstick = "+" // vcat center1 (replicate (rows x) "|") // "+"
     hstick = hcat center1 (replicate (cols x) "-")
 
-tabulate :: Show a => Int -> Int -> Int -> Int -> (Box -> Box) -> Zipper a -> Box
-tabulate cellX cellY leftwards rightwards decorate
+box :: String -> Box
+box [ ] = nullBox
+box s = vcat left . fmap (alignHoriz left width . text) $ xs
+  where
+    xs = lines s
+    width = (maximum . fmap length) xs
+
+tabulate :: (a -> String) -> Int -> Int -> Int -> Int -> (Box -> Box) -> Zipper a -> Box
+tabulate show' cellX cellY leftwards rightwards decorate
     = hsep 1 center1 . straighten
     . padLeft leftwards (normalize' nullBox) . padRight rightwards (normalize' nullBox)
     . fmap (decorate . normalize . text)
-    . fmap show
+    . fmap show'
   where normalize  = align center1 center1 cellY cellX
         normalize' = align center1 center1 (cellY + (rows . decorate) nullBox)
                                                 (cellX + (cols . decorate) nullBox)
@@ -49,28 +66,59 @@ stepLeft, stepRight :: Zipper a -> Zipper a
 stepLeft  Zipper{..} = Zipper { lefts  = tail lefts , focus = head lefts , rights = focus: rights }
 stepRight Zipper{..} = Zipper { rights = tail rights, focus = head rights, lefts  = focus: lefts  }
 
+growLeft, growRight :: a -> Zipper a -> Zipper a
+growLeft  x z@Zipper{..} | null lefts  = z { focus = x, rights = focus: rights }
+                         | otherwise = stepLeft  z
+growRight x z@Zipper{..} | null rights = z { focus = x, lefts  = focus: lefts  }
+                         | otherwise = stepRight z
+
 crop :: Int -> Int -> Zipper a -> Zipper a
 crop m n Zipper{..} = Zipper { lefts = take m lefts, focus = focus, rights = take n rights }
 
 straighten :: Zipper a -> [a]
 straighten Zipper{..} = reverse lefts ++ focus: rights
 
-mirror :: [a] -> Maybe (Zipper a)
-mirror [ ] = Nothing
-mirror (x: xs) = Just Zipper { lefts = xs, focus = x, rights = xs }
-
 type Zipper2D a = Zipper (Zipper a)
 
-instance {-# overlapping #-} Show a => Show (Zipper (Zipper a)) where
-    show z = render . vcat left . straighten . fmap boxify $ z
+singleton2d :: a -> Zipper2D a
+singleton2d = singleton . singleton
+
+tabulate2d :: (a -> String) -> Zipper2D a -> String
+tabulate2d show z = render . vcat left . straighten . fmap boxify $ z
       where
         xs = concatMap toList z
         cellX = maximum . fmap (maximum . fmap length . lines . show) $ xs
         cellY = maximum . fmap (               length . lines . show) $ xs
         leftwards  = maximum . fmap (length . lefts ) $ z
         rightwards = maximum . fmap (length . rights) $ z
-        boxify = tabulate cellX cellY leftwards rightwards id
+        boxify = tabulate show cellX cellY leftwards rightwards id
 
-stepUp, stepDown :: Zipper2D a -> Zipper2D a
-stepUp   zs = fmap stepLeft  zs
-stepDown zs = fmap stepRight zs
+
+instance {-# overlapping #-} Show a => Show (Zipper (Zipper a)) where
+    show z = tabulate2d show z
+
+z2 = let (x: xs) = [ crop i (5 - i) integers | i <- [0..5] ] in mirror x xs
+
+stepLeft2D, stepRight2D, stepUp2D, stepDown2D :: Zipper2D a -> Zipper2D a
+stepLeft2D  = fmap stepLeft
+stepRight2D = fmap stepRight
+stepUp2D   = stepLeft
+stepDown2D = stepRight
+
+growLeft2D, growRight2D, growUp2D, growDown2D :: a -> Zipper2D a -> Zipper2D a
+growLeft2D  x = fmap (growLeft  x)
+growRight2D x = fmap (growRight x)
+growUp2D   x = growLeft  (singleton x)
+growDown2D x = growRight (singleton x)
+
+extend :: (Zipper a -> a) -> Zipper a -> Zipper a
+extend f z = z { focus = f z }
+
+extract :: Zipper a -> a
+extract = focus
+
+extend2d :: (Zipper2D a -> a) -> Zipper2D a -> Zipper2D a
+extend2d f z = z { focus = extend (const (f z)) (focus z) }
+
+extract2d :: Zipper2D a -> a
+extract2d = focus . focus
